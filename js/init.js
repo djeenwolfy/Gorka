4a9220f321019e0db5ea7c8603378e2f490ad206
djeenwolfy <volkovpavelvladislavovich@gmail.com>
2019-02-28 15:57:16 +0300

    slider

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+  	 $('.slider').slider({
+  	 	interval:5000,
+  	 });
+    $('.sidenav').sidenav();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

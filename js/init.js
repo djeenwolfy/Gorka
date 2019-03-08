da23c3d9f0189d46f0c198d458669a62259c8350
HornetPlay <KasperP233@gmail.com>
2019-03-08 11:10:31 +0300

    float mobile btn & dropdown menu & responsive catalog 480px

--- js/init.js
+++ js/init.js
@@ -5,12 +5,14 @@
   	 	height: 642
   	 });
     $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
+     // $('.fixed-action-btn').floatingActionButton({
+     //  direction: 'buttom'
+     // });
     VK.Widgets.Group("vk_groups", {mode: 1, width: "400"}, 48288021);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 $(function() {
-
 		$( '#mi-slider' ).catslider();
-
-	});
+});

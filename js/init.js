9ec9b5ea742bf23d4770f20a565d46baaf86dd93
HornetPlay <KasperP233@gmail.com>
2019-03-08 14:45:50 +0300

    add scroll to footer & modals block with map

--- js/init.js
+++ js/init.js
@@ -6,9 +6,8 @@
   	 });
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown();
-     // $('.fixed-action-btn').floatingActionButton({
-     //  direction: 'buttom'
-     // });
+    $('.modal').modal();
+    
     VK.Widgets.Group("vk_groups", {mode: 1, width: "400"}, 48288021);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
